65f0ca4e424eedcc85a258802906309449aeb158
neil <github@neilpang.com>
2022-07-18 18:17:23 +0800

    new method

--- index.js
+++ index.js
@@ -1,141 +1,40 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
-const tc = require('@actions/tool-cache');
-const io = require('@actions/io');
 const fs = require("fs");
 const path = require("path");
 
-async function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms))
-}
+
+var workingDir = __dirname;
 
 async function execSSH(cmd, desp = "") {
   core.info(desp);
   core.info("exec ssh: " + cmd);
-  await exec.exec("ssh -t solaris", [], { input: cmd });
+  await exec.exec("bash " + workingDir + "/run.sh execSSH", [], { input: cmd });
 }
 
-
-async function getScreenText(vmName) {
-  let png = path.join(__dirname, "/screen.png");
-  await vboxmanage(vmName, "controlvm", "screenshotpng  " + png);
-  await exec.exec("sudo chmod 666 " + png);
-  let output = "";
-  await exec.exec("pytesseract  " + png, [], {
-    listeners: {
-      stdout: (s) => {
-        output += s;
-      }
-    }
-  });
-  return output;
+async function execSSHSH(cmd, desp = "") {
+  core.info(desp);
+  core.info("exec ssh: " + cmd);
+  await exec.exec("bash " + workingDir + "/run.sh execSSHSH", [], { input: cmd });
 }
 
-async function waitFor(vmName, tag) {
-
-  let slept = 0;
-  while (true) {
-    slept += 1;
-    if (slept >= 300) {
-      throw new Error("Timeout can not boot");
-    }
-    await sleep(1000);
-
-    let output = await getScreenText(vmName);
-
-    if (tag) {
-      if (output.includes(tag)) {
-        core.info("OK");
-        await sleep(1000);
-        return true;
-      } else {
-        core.info("Checking, please wait....");
-      }
-    } else {
-      if (!output.trim()) {
-        core.info("OK");
-        return true;
-      } else {
-        core.info("Checking, please wait....");
-      }
-    }
-
+async function shell(cmd, cdToScriptHome = true) {
+  core.info("exec shell: " + cmd);
+  if(cdToScriptHome) {
+    await exec.exec("bash", [], { input: "cd " + workingDir + " && (" + cmd + ")" });
+  } else {
+    await exec.exec("bash", [], { input:  cmd  });
   }
 
-  return false;
+
 }
 
-
-async function vboxmanage(vmName, cmd, args = "") {
-  await exec.exec("sudo  vboxmanage " + cmd + "   " + vmName + "   " + args);
-}
 
 async function setup(nat, mem) {
   try {
 
-    let sshport = 2223;
-    fs.appendFileSync(path.join(process.env["HOME"], "/.ssh/config"), "Host solaris " + "\n");
-    fs.appendFileSync(path.join(process.env["HOME"], "/.ssh/config"), " User root" + "\n");
-    fs.appendFileSync(path.join(process.env["HOME"], "/.ssh/config"), " HostName localhost" + "\n");
-    fs.appendFileSync(path.join(process.env["HOME"], "/.ssh/config"), " Port " + sshport + "\n");
-    fs.appendFileSync(path.join(process.env["HOME"], "/.ssh/config"), "StrictHostKeyChecking=accept-new\n");
+    await shell("bash run.sh importVM");
 
-
-    let workingDir = __dirname;
-
-//    if (process.env["DEBUG"]) {
-//      let sdk = "https://download.virtualbox.org/virtualbox/6.1.14/Oracle_VM_VirtualBox_Extension_Pack-6.1.14.vbox-extpack";
-//      core.info("Downloading sdk: " + sdk);
-//      let img = await tc.downloadTool(sdk);
-//      core.info("Downloaded file: " + img);
-//      await io.mv(img, path.join(workingDir, "./Oracle_VM_VirtualBox_Extension_Pack-6.1.14.vbox-extpack"));
-//      await exec.exec("sudo vboxmanage extpack install    --replace " + path.join(workingDir, "./Oracle_VM_VirtualBox_Extension_Pack-6.1.14.vbox-extpack"), [], { input: "y\n" });
-//
-//
-//      let ng = await tc.downloadTool("https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip");
-//
-//      let token = process.env["NGROK_TOKEN"];
-//      await io.mv(ng, path.join(workingDir, "./ngrok-stable-darwin-amd64.zip"));
-//      await exec.exec("unzip -o " + path.join(workingDir, "./ngrok-stable-darwin-amd64.zip"));
-//      await exec.exec("./ngrok authtoken " + token);
-//      exec.exec("./ngrok  tcp   3390").catch((e) => {
-//        //
-//      });
-//    }
-
-
-    core.info("Install tesseract");
-    await exec.exec("brew install tesseract");
-    await exec.exec("pip3 install pytesseract");
-
-
-    let imgName = "sol-11_4-vbox";
-    let ova = imgName + ".ova";
-    let part0 = "https://github.com/vmactions/solaris-builder/releases/download/v0.0.4/sol-11_4-vbox.ova.zip";
-    let part1 = "https://github.com/vmactions/solaris-builder/releases/download/v0.0.4/sol-11_4-vbox.ova.z01";
-
-
-    {
-      core.info("Downloading image: " + part0);
-      let img = await tc.downloadTool(part0);
-      core.info("Downloaded file: " + img);
-      await io.mv(img, path.join(workingDir, "./" + ova + ".zip"));
-
-    }
-
-    {
-      core.info("Downloading image: " + part1);
-      let img = await tc.downloadTool(part1);
-      core.info("Downloaded file: " + img);
-      await io.mv(img, path.join(workingDir, "./" + ova + ".z01"));
-    }
-
-    await exec.exec("7za e -y " + path.join(workingDir, ova + ".zip") + "  -o" + workingDir);
-
-    await vboxmanage("", "import", path.join(workingDir, ova));
-
-    //the ssh port was already added in the builder.
-    //await vboxmanage(imgName, "modifyvm", '--natpf1 "guestssh,tcp,,' + sshport + ',,22"');
 
     if (nat) {
       let nats = nat.split("\n").filter(x => x !== "");
@@ -147,59 +46,67 @@ async function setup(nat, mem) {
           let proto = segs[0].trim().trim('"');
           let hostPort = segs[1].trim().trim('"');
           let vmPort = segs[2].trim().trim('"');
-          await vboxmanage(imgName, "modifyvm", "  --natpf1 '" + hostPort + "," + proto + ",," + hostPort + ",," + vmPort + "'");
+
+          await shell("bash run.sh addNAT " + proto + " " + hostPort + " " + vmPort);
 
         } else if (segs.length === 2) {
           let proto = "tcp"
           let hostPort = segs[0].trim().trim('"');
           let vmPort = segs[1].trim().trim('"');
-          await vboxmanage(imgName, "modifyvm", "  --natpf1 '" + hostPort + "," + proto + ",," + hostPort + ",," + vmPort + "'");
+          await shell("bash run.sh addNAT " + proto + " " + hostPort + " " + vmPort);
         }
       };
     }
+
     if (mem) {
-      await vboxmanage(imgName, "modifyvm", "  --memory " + mem);
+      await shell("bash run.sh setMemory " + mem);
     }
-    await vboxmanage(imgName, "modifyvm", "  --cpus 3 ");
 
-    await vboxmanage(imgName, "startvm", " --type headless");
+    await shell("bash run.sh setCPU  3");
 
-    await waitFor(imgName, "Loading NVIDIA Kernel Mode Setting Driver for UNIX platforms");
-    await sleep(1000);
+    await shell("bash run.sh startVM " );
 
-    await waitFor(imgName, "Hostname: solaris");
-    await sleep(1000);
+    core.info("First boot");
 
 
-    let sshHome = path.join(process.env["HOME"], ".ssh");
-    let authorized_keys = path.join(sshHome, "authorized_keys");
 
-    fs.appendFileSync(authorized_keys, fs.readFileSync(path.join(workingDir, "id_rsa.pub")));
-
-    fs.appendFileSync(path.join(sshHome, "config"), "SendEnv   CI  GITHUB_* \n");
-    await exec.exec("chmod 700 " + sshHome);
-
+    await shell("bash run.sh waitForLoginTag");
 
 
     let cmd1 = "mkdir -p /Users/runner/work && ln -s /Users/runner/work/  work";
     await execSSH(cmd1, "Setting up VM");
 
-    await exec.exec("rsync -auvzrtopg  --exclude _actions/vmactions/solaris-vm  /Users/runner/work/ solaris:work");
-
+    let sync = core.getInput("sync");
+    if (sync == "sshfs") {
+      core.info("Setup sshfs");
+      await shell("bash run.sh runSSHFSInVM");
+    } else {
+      await shell("bash run.sh installRsyncInVM");
+      await shell("bash run.sh rsyncToVM");
+    }
 
     core.info("OK, Ready!");
 
   }
   catch (error) {
+    await shell("pwd && ls -lah" );
+    await shell("bash -c 'pwd && ls -lah ~/.ssh/ && cat ~/.ssh/config'" );
     core.setFailed(error.message);
   }
 }
 
 
 
-
-
 async function main() {
+  let debug = core.getInput("debug");
+  process.env.DEBUG = debug;
+  let release = core.getInput("release");
+  core.info("release: " + release);
+  if(release) {
+    process.env.VM_RELEASE=release;
+  }
+
+
   let nat = core.getInput("nat");
   core.info("nat: " + nat);
 
@@ -210,6 +117,7 @@ async function main() {
 
   var envs = core.getInput("envs");
   console.log("envs:" + envs);
+
   if (envs) {
     fs.appendFileSync(path.join(process.env["HOME"], "/.ssh/config"), "SendEnv " + envs + "\n");
   }
@@ -221,19 +129,38 @@ async function main() {
   var prepare = core.getInput("prepare");
   if (prepare) {
     core.info("Running prepare: " + prepare);
-    await exec.exec("ssh -t solaris", [], { input: prepare });
+    await execSSH(prepare);
   }
 
   var run = core.getInput("run");
   console.log("run: " + run);
 
+  var error = null;
   try {
-    await exec.exec("ssh solaris sh -c 'cd $GITHUB_WORKSPACE && exec sh'", [], { input: run });
-  } catch (error) {
-    core.setFailed(error.message);
+    var usesh = core.getInput("usesh").toLowerCase() == "true";
+    if (usesh) {
+      await execSSHSH("cd $GITHUB_WORKSPACE && (" + run +")");
+    } else {
+      await execSSH("cd $GITHUB_WORKSPACE && (" + run +")");
+    }
+
+  } catch (err) {
+    error = err;
   } finally {
-    core.info("get back by rsync");
-    await exec.exec("rsync -uvzrtopg  solaris:work/ /Users/runner/work");
+    let copyback = core.getInput("copyback");
+    if(copyback !== "false") {
+      let sync = core.getInput("sync");
+      if (sync != "sshfs") {
+        core.info("get back by rsync");
+        await exec.exec("bash " + workingDir + "/run.sh rsyncBackFromVM");
+      }
+    }
+    if(error) {
+      core.setFailed(error.message);
+      process.exit(1);
+    } else {
+      process.exit(0);
+    }
   }
 }
 
